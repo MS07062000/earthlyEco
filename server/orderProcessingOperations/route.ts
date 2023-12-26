ca100c2d69a9d2409ea65b1f4b5f8346481b960c
MS07062000 <singhmurali.14@gmail.com>
2023-12-26 12:49:21 +0530

    testing

--- server/orderProcessingOperations/route.ts
+++ server/orderProcessingOperations/route.ts
@@ -22,9 +22,9 @@ router.use('/createOrder', async (req: Request, res: Response) => {
 
 router.use('/orderPaid', async (req: Request, res: Response) => {
     try {
-        console.log(JSON.stringify(req.body));
+        // console.log(JSON.stringify(req.body));
         console.log(req.headers['x-razorpay-signature']);
-        console.log(req.toString());
+        console.log(JSON.stringify(req));
         const response = await orderPaid(req);
         console.log(response);
         res.status(200).send(response);

382127e83eda1a482bae3fe8214878063f4646fc
MS07062000 <singhmurali.14@gmail.com>
2023-12-26 12:58:08 +0530

    logging headers

--- server/orderProcessingOperations/route.ts
+++ server/orderProcessingOperations/route.ts
@@ -24,7 +24,7 @@ router.use('/orderPaid', async (req: Request, res: Response) => {
     try {
         // console.log(JSON.stringify(req.body));
         console.log(req.headers['x-razorpay-signature']);
-        console.log(JSON.stringify(req));
+        console.log(JSON.stringify(req.headers));
         const response = await orderPaid(req);
         console.log(response);
         res.status(200).send(response);

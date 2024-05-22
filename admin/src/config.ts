cbcec145915c15051afc1150691f99cf63804d2a
MS07062000 <singhmurali.14@gmail.com>
2024-05-23 01:32:00 +0530

    FIxing admin API base URL

--- admin/src/config.ts
+++ admin/src/config.ts
@@ -1,5 +1,5 @@
 export const isDevelopmentEnvironment = import.meta.env.VITE_APP_ENVIRONMENT as string === "development";
 
-export const BASEURL = isDevelopmentEnvironment ? import.meta.env.VITE_DEVELOPMENT_BASEURL as string : import.meta.env.VITE_ADMIN_PRODUCTION_BASEURL as string;
+export const BASEURL = isDevelopmentEnvironment ? import.meta.env.VITE_DEVELOPMENT_BASEURL as string : import.meta.env.VITE_PRODUCTION_BASEURL as string;
 
-export const ORIGIN = isDevelopmentEnvironment ? 'http://localhost:5173' : 'https://maniecommercestore.web.app';
+export const ORIGIN = isDevelopmentEnvironment ? 'http://localhost:5173' : import.meta.env.VITE_ADMIN_PRODUCTION_BASEURL as string;
